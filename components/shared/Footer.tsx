424d68bd2024270c0439431ca2908ab607ff508f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-01-10 13:48:36 -0500

    Completed Footer component

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className='border-t'>
+      <div className='flex flex-col gap-4 p-5 flex-center wrapper flex-between text-center sm:flex-row'>
+        <Link href='/'>
+          <Image
+            src='/assets/images/logo.svg'
+            width={128}
+            height={38}
+            alt='Evently Logo'
+          />
+        </Link>
+
+        <p>2024 Evently. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
